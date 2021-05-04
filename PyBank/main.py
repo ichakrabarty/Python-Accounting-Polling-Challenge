3e194a8f978f11893cf3a9cfa1d8e79441e69154
BadHacker <ishan.chak@gmail.com>
2021-05-04 18:56:31 -0500

    added Total Price/Losses script to PyBank

--- PyBank/main.py
+++ PyBank/main.py
@@ -12,12 +12,18 @@ with open(budget_path) as budget_file:
     print(f"Header: {budget_header}")
     
     months = 0 #set counter for amount of months
+    budget_total = 0 #set counter for total Profit/Losses
     
-    for budget_row in budget_reader: #reads each ro in csv file
-        print(budget_row)
+    for budget_row in budget_reader: #reads each row in csv file
+        budget_list = list(budget_row)
+        print(budget_list)
+        
         months = months + 1
+        budget_total = int(budget_list[1]) + budget_total
+         
+    
     print(f'Total Months: {months}') #Months in datasheet
-
+    print(f'Total: ${budget_total}') #Total Profit/Losses
 
     
 
